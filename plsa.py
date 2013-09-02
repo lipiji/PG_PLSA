c488f87b515d0294cd1e106d196ff02d7b5db27a
Piji Li <pagelee.sd@gmail.com>
2013-09-02 09:57:54 +0800

    stemmer

--- plsa.py
+++ plsa.py
@@ -18,72 +18,75 @@ import re
 import numpy as np
 from random import random
 from operator import itemgetter
+import utils.stemmer as stemmer 
 
 def normalize(vec):
-    s = sum(vec)
-    assert(abs(s) != 0.0) # the sum must not be 0
-    
-    for i in range(len(vec)):
-        assert(vec[i] >= 0) # element must be >= 0
-        vec[i] = vec[i] * 1.0 / s
-       
+	s = sum(vec)
+	assert(abs(s) != 0.0) # the sum must not be 0
+
+	for i in range(len(vec)):
+		assert(vec[i] >= 0) # element must be >= 0
+		vec[i] = vec[i] * 1.0 / s
+
 
 # stop words
 STOP_WORDS_DIC = set()
 def load_stop_words(sw_file_path):
-    sw_file = open(sw_file_path, "r")
-    for word in sw_file:
-        word = word.replace("\n", "")
-        word = word.replace("\r\n", "")
-        STOP_WORDS_DIC.add(word)
-    sw_file.close()
+	sw_file = open(sw_file_path, "r")
+	for word in sw_file:
+		word = word.replace("\n", "")
+		word = word.replace("\r\n", "")
+		STOP_WORDS_DIC.add(word)
+	sw_file.close()
 
 # di
-class Document(object):        
-    PUNCTUATION = ['(', ')', ':', ';', ',', '-', '!', '.', '?', '/', '"', '*']
-    CARRIAGE_RETURNS = ['\n', '\r\n']
-    WORD_REGEX = "^[a-z']+$"
+class Document(object):
+	PUNCTUATION = ['(', ')', ':', ';', ',', '-', '!', '.', '?', '/', '"', '*']
+	CARRIAGE_RETURNS = ['\n', '\r\n']
+	WORD_REGEX = "^[a-z']+$"
 
-    def __init__(self, filepath):
-        self.filepath = filepath
-        self.file = open(self.filepath)
-        self.lines = []
-        self.words = []
+	def __init__(self, filepath):
+		self.filepath = filepath
+		self.file = open(self.filepath)
+		self.lines = []
+		self.words = []
 
-    def split(self, STOP_WORDS_DIC):
-        self.lines = [line for line in self.file]
-        for line in self.lines:
-            words = line.split(' ')
-            for word in words:
-                clean_word = self._clean_word(word)
-                if clean_word and (clean_word not in STOP_WORDS_DIC) and (len(clean_word) > 1):
-                    self.words.append(clean_word)
-        self.file.close()
+	def split(self, STOP_WORDS_DIC):
+		self.lines = [line for line in self.file]
+		for line in self.lines:
+			words = line.split(' ')
+			for word in words:
+				clean_word = self._clean_word(word)
+				if clean_word and (clean_word not in STOP_WORDS_DIC) and (len(clean_word) > 1):
+					self.words.append(clean_word)
+		self.file.close()
 
-
-    def _clean_word(self, word):
-        word = word.lower()
-        for punc in Document.PUNCTUATION + Document.CARRIAGE_RETURNS:
-            word = word.replace(punc, '').strip("'")
-        return word if re.match(Document.WORD_REGEX, word) else None
+	def _clean_word(self, word):
+		word = word.lower()
+		for punc in Document.PUNCTUATION + Document.CARRIAGE_RETURNS:
+			word = word.replace(punc, '').strip("'")
+			# stemmer: dogs -> dog ; created -> creat
+			ps = stemmer.PorterStemmer()
+			word = ps.stem(word, 0, len(word)-1)
+		return word if re.match(Document.WORD_REGEX, word) else None
 
 #D
 class Corpus(object):
-    def __init__(self):
-        self.documents = []
+	def __init__(self):
+		self.documents = []
 
 
-    def add_document(self, document):
-        self.documents.append(document)
+	def add_document(self, document):
+		self.documents.append(document)
 
-    def build_vocabulary(self):
-        discrete_set = set()
-        for document in self.documents:
-            for word in document.words:
-                discrete_set.add(word)
-        self.vocabulary = list(discrete_set)
-        
-		
+	def build_vocabulary(self):
+		discrete_set = set()
+		for document in self.documents:
+			for word in document.words:
+				discrete_set.add(word)
+		self.vocabulary = list(discrete_set)
+
+
 class Plsa(object):
 	def __init__(self, corpus, number_of_topics, max_iter, model_path):
 		self.n_d = len(corpus.documents)
@@ -135,7 +138,7 @@ class Plsa(object):
 				f.write(" "+self.p_z_d[di, zi])
 			f.write("\n")
 		f.close()
-	
+
 	def print_p_w_z(self):
 		filename = self.model_path + "p_w_z.txt"
 		f = open(filename, "w")
@@ -168,9 +171,9 @@ class Plsa(object):
 
 			# likelihood
 			self.L = self.log_likelihood()
-			
+
 			self.print_top_words(10)
-			
+
 			print "Iter " + str(i_iter) + ", L=" + str(self.L)
 
 			print "E-Step..."
@@ -211,32 +214,32 @@ class Plsa(object):
 					self.p_w_z[zi, wi] = sum2[wi] / sum1
 
 def main(argv):
-    print "Usage: python ./plsa.py <number_of_topics> <maxiteration>"
+	print "Usage: python ./plsa.py <number_of_topics> <maxiteration>"
 
-    # load stop words
-    load_stop_words("./data/stopwords.txt")
+	# load stop words
+	load_stop_words("./data/stopwords.txt")
 
-    corpus = Corpus()
-    document_paths = ['./data/events_2010/GulfOilSpill/', './data/events_2010/HaitiEarthquake/',
-                      './data/events_2010/MichaelJacksonDied/', './data/events_2010/PakistanFloods/',
-                      './data/events_2010/RussianForestFires/', './data/events_2010/SouthAfricaWorldCup/']
+	corpus = Corpus()
+	document_paths = ['./data/events_2010/GulfOilSpill/', './data/events_2010/HaitiEarthquake/',
+			'./data/events_2010/MichaelJacksonDied/', './data/events_2010/PakistanFloods/',
+			'./data/events_2010/RussianForestFires/', './data/events_2010/SouthAfricaWorldCup/']
 
-    #document_paths = ['./texts/grimm_fairy_tales', './texts/tech_blog_posts', './texts/nyt']
-    
-    for document_path in document_paths:
-        for document_file in glob.glob(os.path.join(document_path, '*.txt')):
-            document = Document(document_file)
-            document.split(STOP_WORDS_DIC)
-            corpus.add_document(document)
+	#document_paths = ['./texts/grimm_fairy_tales', './texts/tech_blog_posts', './texts/nyt']
+
+	for document_path in document_paths:
+		for document_file in glob.glob(os.path.join(document_path, '*.txt')):
+			document = Document(document_file)
+			document.split(STOP_WORDS_DIC)
+			corpus.add_document(document)
 	corpus.build_vocabulary()
-    
 
-    number_of_topics = 12 #int(argv[1])
-    max_iterations = 100 #int(argv[2])
-    plsa = Plsa(corpus, number_of_topics, max_iterations, "./model/")
-    plsa.train()   
+
+	number_of_topics = 12 #int(argv[1])
+	max_iterations = 100 #int(argv[2])
+	plsa = Plsa(corpus, number_of_topics, max_iterations, "./model/")
+	plsa.train()   
 
 if __name__ == "__main__":
-    main(sys.argv)
+	main(sys.argv)
 
 
